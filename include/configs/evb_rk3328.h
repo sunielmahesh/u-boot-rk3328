03c13c824bf63bde3cf39f62d59e8d88b4553e9d
Suniel Mahesh <sunil@amarulasolutions.com>
2022-10-27 16:50:54 +0530

    include: configs: Add ROCKCHIP_DEVICE_SETTINGS
    Signed-off-by: Suniel Mahesh <sunil@amarulasolutions.com>

--- include/configs/evb_rk3328.h
+++ include/configs/evb_rk3328.h
@@ -6,6 +6,11 @@
 #ifndef __EVB_RK3328_H
 #define __EVB_RK3328_H
 
+#define ROCKCHIP_DEVICE_SETTINGS \
+                "stdin=serial,usbkbd\0" \
+                "stdout=serial,vidconsole\0" \
+                "stderr=serial,vidconsole\0"
+
 #include <configs/rk3328_common.h>
 
 #define SDRAM_BANK_SIZE			(2UL << 30)
